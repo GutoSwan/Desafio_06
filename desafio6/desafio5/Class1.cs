7ab6986340040c0ff7e4c0733dcc6c83360f5009
GutoSwan <gubuenosa@gmail.com>
2022-04-09 14:34:30 -0300

    desafio6 concluido

---
+++ desafio6/desafio5/Class1.cs
@@ -0,0 +1,23 @@
+﻿using desafio5;
+
+
+
+Aluno orlandoAluno = new Aluno("orlando", 12, 5);
+
+Aluno scobyAluno = new Aluno("scoby", 15, 4);
+
+Aluno chubsAluno = new Aluno("chubs", 23, 8);
+
+List<Aluno> alunos = new List<Aluno>();
+alunos.Add(orlandoAluno);
+alunos.Add(scobyAluno);
+alunos.Add(chubsAluno);
+
+
+foreach (var aluninho in alunos)
+{
+    StreamWriter sw = new StreamWriter("D:\\desafi C#\\desafio5\\"+ aluninho.nome + ".csv");
+    sw.WriteLine(aluninho.nome + ";" + aluninho.idade + ";" + aluninho.nota);
+    sw.Close();
+}
+//sw.WriteLine("soma das notas: " + ";" + alunos.Select(x => x.nota).Sum());
